a2eec408414bec891250bc67e3b97e9ee27ca196
Patrick <patrick.oliveira@aluno.ufabc.edu.br>
2021-04-26 06:53:49 -0300

    .

---
+++ examples/boids3d/main.cpp
@@ -0,0 +1,22 @@
+#include <fmt/core.h>
+
+#include "abcg.hpp"
+#include "openglwindow.hpp"
+
+int main(int argc, char **argv) {
+  try {
+    abcg::Application app(argc, argv);
+
+    auto window{std::make_unique<OpenGLWindow>()};
+    window->setOpenGLSettings({.samples = 4});
+    window->setWindowSettings(
+        {.width = 1600, .height = 1000,
+         .showFPS = false, .showFullscreenButton = false, .title = "boids3d"});
+
+    app.run(window);
+  } catch (abcg::Exception &exception) {
+    fmt::print(stderr, "{}\n", exception.what());
+    return -1;
+  }
+  return 0;
+}
